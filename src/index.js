8ee706f9c9092b4a5fce67263c4fa4da244094b1
Hongbusi <q951686662@qq.com>
2022-05-05 09:21:47 +0800

    chore(release): v0.0.9

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+const fs = require('fs')
+const { resolve } = require('path')
 const args = require('minimist')(process.argv.slice(2))
 const chalk = require('chalk')
 const semver = require('semver')
 const { prompt } = require('enquirer')
 const execa = require('execa')
-
-const { getPackageJson, setPackageJson } = require('./fs')
 
 const pkg = getPackageJson()
 
@@ -151,6 +151,36 @@ async function publishPackage(version) {
     }
     else {
       throw e
+    }
+  }
+}
+
+function getPackageJson(cwd = process.cwd()) {
+  const path = resolve(cwd, 'package.json')
+
+  if (fs.existsSync(path)) {
+    try {
+      const raw = fs.readFileSync(path, 'utf-8')
+      const data = JSON.parse(raw)
+      return data
+    }
+    catch (e) {
+      console.log(chalk.red('Failed to parse package.json.'))
+      process.exit(0)
+    }
+  }
+}
+
+function setPackageJson(data, cwd = process.cwd()) {
+  const path = resolve(cwd, 'package.json')
+
+  if (fs.existsSync(path)) {
+    try {
+      fs.writeFileSync(path, JSON.stringify(data, null, 2))
+    }
+    catch (e) {
+      console.log(chalk.red('Failed to parse package.json.'))
+      process.exit(0)
     }
   }
 }
